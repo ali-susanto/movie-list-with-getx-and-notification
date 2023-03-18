9ee254dbbb0012982c63252a4ae4f35b2143fea3
Ali Susanto <alisusanto.main@gmail.com>
2023-03-18 11:01:12 +0700

    add now playing movie and refactor getMovie

--- lib/app/api/api_provider.dart
+++ lib/app/api/api_provider.dart
@@ -3,8 +3,14 @@ import 'package:movie_list_getx/app/utils/constant.dart';
 
 class ApiProvider {
   final dio = Dio(BaseOptions(baseUrl: baseUrl));
-  Future<Response> getMovie() async {
+  Future<Response> getUpcomingMovie() async {
     final response = await dio.get('/movie/upcoming',
+        options: Options(headers: {'Authorization': 'Bearer $bearerToken'}));
+    return response;
+  }
+
+  Future<Response> getNowPlayingMovie() async {
+    final response = await dio.get('/movie/now_playing',
         options: Options(headers: {'Authorization': 'Bearer $bearerToken'}));
     return response;
   }
